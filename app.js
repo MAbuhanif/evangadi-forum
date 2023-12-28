85f141cd3265da4d89007b985b0284145dc88e1f
MAbuhanif <mundinoabete@gmail.com>
2023-12-28 16:27:16 +0000

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const mysql2 = require("mysql2")
+const express = require("express");
+const app = express();
+const port = 5500;
+
+
+const userRoutes = require("./routes/userRoute");//user route middleware file
+
+
+app.use("/api/users", userRoutes);//user route middleware
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(`listining on ${port}`);
+  }
+});
